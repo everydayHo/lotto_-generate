d3163ea84344c0ea1050bc067b3d048f643b63ec
everydayHo <jungwoopage@gmail.com>
2023-04-22 21:01:28 +0900

    고정수,선택수중복선택시 알림창,다시 선택시배열빼기

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ const closeBtn = document.querySelector('.overlay .inner .close');
 const fixballBtn = document.querySelector('.fixball_container .btn');
 const choiceBtn = document.querySelector('.choice_container .btn');
 const fixballContainer = document.querySelector('.fixball_container .wrapper');
-
+const selectballContainer = document.querySelector(
+	'.choice_container .wrapper'
+);
+const generateContainer = document.querySelector(
+	'.generate_container .wrapper'
+);
+const generateBtn = document.querySelector('.generate_container .makebtn');
+const regenerateBtn = document.querySelector('.generate_container .refreshbtn');
+const loadingContainer = document.querySelector(
+	'.generate_container .loading-container'
+);
 // 팝업창 공 나오게 만듬
 function createItem(number) {
 	const itemrow = document.createElement('span');
@@ -28,11 +38,9 @@ let uniqueNumbers;
 //  고정수를 만듬
 function getFixball(e) {
 	popup.classList.toggle('visible');
-	if (clickNumber2) {
-		overlayContainer.removeEventListener('click', clickNumber2);
-		popupSubmitBtn.removeEventListener('click', () => {
-			console.log('넘어오는 값2:', otherNumbers);
-		});
+	if (clickSelectNumber) {
+		overlayContainer.removeEventListener('click', clickSelectNumber);
+		popupSubmitBtn.removeEventListener('click', selectballSubmitHandler);
 	}
 	overlayContainer.addEventListener('click', clickNumber);
 }
@@ -42,29 +50,45 @@ function getSelectball(e) {
 	popup.classList.toggle('visible');
 	if (clickNumber) {
 		overlayContainer.removeEventListener('click', clickNumber);
-		popupSubmitBtn.removeeventListener('click', fixballSubmitHandler);
+		popupSubmitBtn.removeEventListener('click', fixballSubmitHandler);
 	}
-	overlayContainer.addEventListener('click', clickNumber2);
+	overlayContainer.addEventListener('click', clickSelectNumber);
 }
 function clickNumber(e) {
 	if (e.target.classList.contains('ball')) {
 		let selectedNumber = parseInt(e.target.innerText);
-		if (selectedNumbers.indexOf(selectedNumber) === -1) {
+		if (
+			selectedNumbers.indexOf(selectedNumber) === -1 &&
+			!otherNumbers.includes(selectedNumber)
+		) {
 			e.target.classList.toggle('fixballs');
 			selectedNumbers.push(selectedNumber);
 		} else {
-			const index = selectedNumbers.indexOf(selectedNumber);
-			selectedNumbers.splice(index, 1);
-			e.target.classList.toggle('fixballs');
+			if (!otherNumbers.includes(selectedNumber)) {
+				const index = selectedNumbers.indexOf(selectedNumber);
+				selectedNumbers.splice(index, 1);
+				e.target.classList.toggle('fixballs');
+			}
 		}
 		if (selectedNumbers.length > 5) {
 			Swal.fire({
 				icon: 'error',
 				title: '욕심쟁이',
 				text: '고정수는 5개 이하까지 선택하세요.',
+			}).then(function () {
+				selectedNumbers.pop();
+				e.target.classList.toggle('fixballs');
+			});
+		}
+		if (e.target.classList.contains('selectballs')) {
+			Swal.fire({
+				icon: 'error',
+				title: '선택수클릭',
+				text: '선택수 수정은 선택수 지정에서 하세요.',
 			});
 		}
 	}
+
 	// 로컬히스토리 저장해야됨
 	console.log(selectedNumbers);
 
@@ -72,6 +96,7 @@ function clickNumber(e) {
 }
 
 const fixballSubmitHandler = () => {
+	fixballContainer.innerHTML = '';
 	selectedNumbers
 		.sort((a, b) => a - b)
 		.forEach((el) => {
@@ -81,19 +106,55 @@ const fixballSubmitHandler = () => {
 			popup.classList.remove('visible');
 		});
 };
-function clickNumber2(e) {
+function clickSelectNumber(e) {
 	if (e.target.classList.contains('ball')) {
 		let otherNumber = parseInt(e.target.innerText);
-		if (otherNumbers.indexOf(otherNumber) === -1) {
-			e.target.style.backgroundColor = '#00c6cf';
+		if (
+			otherNumbers.indexOf(otherNumber) === -1 &&
+			!selectedNumbers.includes(otherNumber)
+		) {
+			e.target.classList.toggle('selectballs');
 			otherNumbers.push(otherNumber);
+		} else {
+			if (!selectedNumbers.includes(otherNumber)) {
+				const index = otherNumbers.indexOf(otherNumber);
+				otherNumbers.splice(index, 1);
+				e.target.classList.toggle('selectballs');
+			}
 		}
+		if (e.target.classList.contains('fixballs')) {
+			Swal.fire({
+				icon: 'error',
+				title: '고정수 클릭',
+				text: '고정수 수정은 고정수 지정에서 하세요.',
+			});
+		}
+
+		console.log('셀렉트:', otherNumbers);
 	}
 
-	popupSubmitBtn.addEventListener('click', () => {
-		console.log('넘어오는 값2:', otherNumbers);
-	});
+	popupSubmitBtn.addEventListener('click', selectballSubmitHandler);
 }
+
+const selectballSubmitHandler = () => {
+	selectballContainer.innerHTML = '';
+	if (otherNumbers.length < 6) {
+		Swal.fire({
+			icon: 'error',
+			title: '선택수가 작아요',
+			text: '고정수없이는 선택수가 5개 이상이여야합니다.',
+		});
+	} else {
+		otherNumbers
+			.sort((a, b) => a - b)
+			.forEach((el) => {
+				let otherBalls = createItem(el);
+				otherBalls.classList.add(colorClass(el));
+				selectballContainer.appendChild(otherBalls);
+				popup.classList.remove('visible');
+			});
+	}
+};
 
 fixballBtn.addEventListener('click', getFixball);
 
@@ -103,17 +164,57 @@ closeBtn.addEventListener('click', () => {
 	popup.classList.remove('visible');
 });
 
-// otherNumbers = [15, 20, 25, 45, 31, 33, 36, 40, 16, 29];
-// generateLottoNumbers(selectedNumbers, otherNumbers);
-// for (let i = 0; i < uniqueNumbers.length; i++) {
-// 	console.log(i + 1 + '번째: ' + uniqueNumbers[i].join(', '));
-// }
+const makeLuckyball = () => {
+	generateContainer.innerHTML = '';
+	if (!generateContainer.hasChildNodes()) {
+		loadingContainer.classList.add('show');
+	} else {
+		loadingContainer.classList.remove('show');
+	}
+	if (otherNumbers.length > 5) {
+		generateLottoNumbers(selectedNumbers, otherNumbers);
+		console.log('유니큐:', uniqueNumbers);
+		for (let i = 0; i < uniqueNumbers.length; i++) {
+			const item = document.createElement('p');
+			item.innerHTML = `<span>${i + 1}번째</span>`;
+			for (let j = 0; j < 6; j++) {
+				const itemIdx = document.createElement('span');
+				itemIdx.setAttribute('class', 'ball');
+				itemIdx.innerText = uniqueNumbers[i][j];
+				itemIdx.classList.add(colorClass(itemIdx.innerText));
+				item.append(itemIdx);
+			}
+			generateContainer.append(item);
+		}
+	}
+};
+generateBtn.addEventListener('click', makeLuckyball);
+
+regenerateBtn.addEventListener('click', () => {
+	Swal.fire(
+		'초기화할까요?',
+		'OK버튼을 클릭하시면 초기화됩니다.',
+		'question'
+	).then(function () {
+		generateContainer.innerHTML = '';
+		selectballContainer.innerHTML = '';
+		fixballContainer.innerHTML = '';
+		selectedNumbers = [];
+		otherNumbers = [];
+		const popupBalls = popup.querySelectorAll('span');
+		popupBalls.forEach((el) => {
+			console.log(el);
+			el.classList.remove('fixballs');
+			el.classList.remove('selectballs');
+		});
+	});
+});
+
 // 고정수와 선택수를 받아서 로또 번호 생성한 다음 정렬
 function generateLottoNumbers(selectedNumbers, otherNumbers) {
 	let allNumbers = [];
 	let val = selectedNumbers.length;
-	console.log('val: ', val);
-	let numberTime = val > 0 ? caseNumber(val) : 500;
+	let numberTime = val > 0 ? caseNumber(val) : 1000;
 
 	for (let i = 0; i < numberTime; i++) {
 		let otherLottoNumbers = generateOtherLottoNumbers(otherNumbers);
@@ -177,7 +278,7 @@ function generateLottoNumbers(selectedNumbers, otherNumbers) {
 			a[4] - b[4] ||
 			a[5] - b[5]
 	);
-	console.log(uniqueNumbers);
+	console.log('안유니큐:', uniqueNumbers);
 	return uniqueNumbers;
 }
 
@@ -200,16 +301,16 @@ function caseNumber(val) {
 	let resultLength = '';
 	switch (val) {
 		case 1:
-			resultLength = 1500;
+			resultLength = 2000;
 			break;
 		case 2:
-			resultLength = 1000;
+			resultLength = 1500;
 			break;
 		case 3:
-			resultLength = 600;
+			resultLength = 1000;
 			break;
 		case 4:
-			resultLength = 200;
+			resultLength = 400;
 			break;
 	}
 	return resultLength;
@@ -224,9 +325,12 @@ function colorClass(ballNumber) {
 			className = 'yellowball';
 			break;
 		case ballNumber < 21:
-			className = 'redball';
+			className = 'blueball';
 			break;
 		case ballNumber < 31:
+			className = 'redball';
+			break;
+		case ballNumber < 41:
 			className = 'grayball';
 			break;
 		case ballNumber < 46:
